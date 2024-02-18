e17cbafdda56a46e003650d0b163d491ea0821b4
NirmalVignu <nirmalvignu@gmail.com>
2024-02-18 13:22:47 +0530

    codes updated

--- src/CNNClassifier/utils/utils.py
+++ src/CNNClassifier/utils/utils.py
@@ -7,11 +7,13 @@ from pathlib import Path
 from typing import Any
 from ensure import ensure_annotations
 from box.exceptions import BoxValueError
-from box import COnfigBox
+from box import ConfigBox
 
 @ensure_annotations
-def read_yaml():
-    pass
+def read_yaml(path_to_yaml:Path):
+    with open(path_to_yaml) as yaml_file:
+        content=yaml.safe_load(yaml_file)
+        return ConfigBox(content)
 
 @ensure_annotations
 def save_json():
@@ -34,6 +36,11 @@ def get_size():
     pass
 
 @ensure_annotations
-def create_directory():
-    pass
+def create_directory(path_to_dir:list,verbose=True):
+    for path in path_to_dir:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at : {path}")
+
+    
 
